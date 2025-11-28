efd17ece5b2929436965001573c066f08d8a7300
Ines <ines@MacBook-Pro-de-Ines.local>
2025-11-28 11:11:31 +0100

    docu modif

---
+++ exemple.py
@@ -0,0 +1,8 @@
+def add(a, b):
+    """Returns the sum of a and b.
+    
+    :param a: First number
+    :param b: Second number
+    :return: Sum of a and b
+    """
+    return a + b
--- source/hello.rst
+++
@@ -1,4 +0,0 @@
-Hello World 
-==================
-
-Hello, this is the BlueObserver documentation!
